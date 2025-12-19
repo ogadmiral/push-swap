ef14d11e7dff0eb192571d3a3e9c45070b43b529
Mustapha Damouh <MustaphaDamouh@outlook.com>
2025-12-19 15:14:46 +0100

    fix: struct conflicting error

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mdamouh <mdamouh@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 15:47:17 by mdamouh           #+#    #+#             */
-/*   Updated: 2025/12/19 10:26:27 by mdamouh          ###   ########.fr       */
+/*   Updated: 2025/12/19 15:14:18 by mdamouh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,5 @@
 
 #include "ft_printf/ft_printf.h"
 #include "libft/libft.h"
-
-typedef struct s_stack {
-	long	*value;
-	void	*next;
-}	t_stack;
 
 #endif
