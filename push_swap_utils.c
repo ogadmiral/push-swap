acb9c5b4c0dd1546474c34e5e6b913995f77d56c
Mustapha Damouh <MustaphaDamouh@outlook.com>
2025-12-19 15:20:25 +0100

    Add: sa, sb & ss

--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: mdamouh <mdamouh@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 10:11:14 by mdamouh           #+#    #+#             */
-/*   Updated: 2025/12/19 10:15:02 by mdamouh          ###   ########.fr       */
+/*   Updated: 2025/12/19 15:19:29 by mdamouh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,22 @@ void	sa(t_stack **stack)
 	first->next = second->next;
 	second->next = first;
 	(*stack) = second;
+}
+
+void	sb(t_stack **stack)
+{
+	t_stack	*first;
+	t_stack	*second;
+
+	first = (*stack);
+	second = (*stack)->next;
+	first->next = second->next;
+	second->next = first;
+	(*stack) = second;
+}
+
+void	ss(t_stack **stack)
+{
+	sa(stack);
+	sb(stack);
 }
