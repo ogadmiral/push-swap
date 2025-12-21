99ea0b292c585756b2b400e9bf758a13c74ac557
Mustapha Damouh <MustaphaDamouh@outlook.com>
2025-12-21 16:01:25 +0100

    Add push_swap_utils_b.c

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@
 #    By: mdamouh <mdamouh@student.1337.ma>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/12/10 15:45:58 by mdamouh           #+#    #+#              #
-#    Updated: 2025/12/21 15:52:11 by mdamouh          ###   ########.fr        #
+#    Updated: 2025/12/21 16:00:25 by mdamouh          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = push_swap
 CC = cc
 CFLAGS = -Wall -Werror -Wextra
-SRCS = push_swap.c push_swap_utils.c
+SRCS = push_swap.c push_swap_utils.c push_swap_utils_b.c
 PRINTF = ./ft_printf/libftprintf.a
 LIBFT = ./libft/libft.a
 
---
+++ push_swap_utils_b.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils_b.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdamouh <mdamouh@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/21 15:57:06 by mdamouh           #+#    #+#             */
+/*   Updated: 2025/12/21 15:59:56 by mdamouh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sb(t_stack **stack)
+{
+	t_stack	*first;
+	t_stack	*second;
+
+	first = (*stack);
+	second = (*stack)->next;
+	first->next = second->next;
+	second->next = first;
+	(*stack) = second;
+	ft_printf("sb\n");
+}
+
+void	pb(t_stack **stacka, t_stack **stackb)
+{
+	t_stack	*heada;
+	t_stack	*headb;
+	t_stack	*seconda;
+
+	heada = *stacka;
+	headb = *stackb;
+	if (!heada)
+		return ;
+	seconda = (*stacka)->next;
+	heada->next = headb;
+	*stackb = heada;
+	*stacka = seconda;
+	ft_printf("pb\n");
+}
+
+void	rb(t_stack **stack)
+{
+	t_stack	*second;
+	t_stack	*head;
+
+	head = *stack;
+	if (!(*stack) || !(head->next))
+		return ;
+	second = head->next;
+	head->next = NULL;
+	*stack = second;
+	ft_lstadd_back(stack, head);
+	ft_printf("rb\n");
+}
