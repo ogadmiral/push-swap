85ba837eed02aef662aa836fd93fd9d34513960d
Mustapha Damouh <MustaphaDamouh@outlook.com>
2025-12-31 21:12:58 +0100

    Fix: Norm

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: mdamouh <mdamouh@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 14:24:26 by mdamouh           #+#    #+#             */
-/*   Updated: 2025/12/31 21:09:35 by mdamouh          ###   ########.fr       */
+/*   Updated: 2025/12/31 21:12:27 by mdamouh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,29 @@
 
 void	ft_applyinst(char *line, t_stack **a, t_stack **b)
 {
-	if(line == "sa\n")
+	if (line == "sa\n")
 		sa(a);
-	else if(line == "sb\n")
+	else if (line == "sb\n")
 		sb(b);
-	else if(line == "ss\n")
+	else if (line == "ss\n")
 		ss(a, b);
-	else if(line == "pa\n")
+	else if (line == "pa\n")
 		pa(b, a);
-	else if(line == "pb\n")
+	else if (line == "pb\n")
 		pb(a, b);
-	else if(line == "ra\n")
+	else if (line == "ra\n")
 		ra(a);
-	else if(line == "rb\n")
+	else if (line == "rb\n")
 		rb(b);
-	else if(line == "rra\n")
+	else if (line == "rra\n")
 		rra(a);
-	else if(line == "rrb\n")
+	else if (line == "rrb\n")
 		rrb(b);
-	else if(line == "rrr\n")
-		rrr(a, b);	
+	else if (line == "rrr\n")
+		rrr(a, b);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	t_stack	*stack;
 	t_stack	*stackb;
@@ -52,10 +52,12 @@ int main(int ac, char **av)
 	}
 	if (is_sorted(stack))
 		return (stack_frier(&stack), 0);
-	while ((line = get_next_line(0)))
+	line = get_next_line(0);
+	while (line)
 	{
 		ft_applyinst(line, &stack, &stackb);
 		free(line);
+		line = get_next_line(0);
 	}
 	return (0);
 }
