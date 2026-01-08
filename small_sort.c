24ee2bef40875f36082e5a2c3c060cb74a992142
Mustapha Damouh <MustaphaDamouh@outlook.com>
2026-01-08 21:03:51 +0100

    Replace exit(0) with return

--- small_sort.c
+++ small_sort.c
@@ -6,7 +6,7 @@
 /*   By: mdamouh <mdamouh@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 16:14:46 by mdamouh           #+#    #+#             */
-/*   Updated: 2026/01/03 00:34:56 by mdamouh          ###   ########.fr       */
+/*   Updated: 2026/01/08 21:02:20 by mdamouh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ void	small_sort(t_stack *stack, t_stack *stackb, int size)
 		if (stack->index > stack->next->index)
 			sa(&stack);
 		stack_frier(&stack);
-		exit(0);
+		return ;
 	}
 	if (size == 3)
 		sort_three(&stack);
